ddc4ff399ab34a5899bed9d4f5eadca7729a3d7d
NikoNi <bluepspower@163.com>
2022-06-18 22:01:07 +0800

    refactor: format codes

--- docs/.pressify/config.ts
+++ docs/.pressify/config.ts
@@ -1,124 +1,124 @@
-import { defineConfig } from 'pressify';
-import type { ThemeConfig } from 'pressify/theme';
+import { defineConfig } from "pressify";
+import type { ThemeConfig } from "pressify/theme";
 
 export default defineConfig<ThemeConfig>({
-  pages: 'src',
+  pages: "src",
   themeConfig: {
-    locale: 'en-US',
-    localeText: 'English',
-    title: 'RUI playground',
-    description: 'The Vite based PoC playground.',
+    locale: "en-US",
+    localeText: "English",
+    title: "RUI playground",
+    description: "The Vite based PoC playground.",
     banner: [
-      'WIP!',
+      "WIP!",
       [
-        'a',
+        "a",
         {
-          href: 'https://patreon.com/nikoni',
-          target: '_blank',
-          style: { marginLeft: '24px' },
+          href: "https://patreon.com/nikoni",
+          target: "_blank",
+          style: { marginLeft: "24px" },
         },
-        'Sponsor this project',
+        "Sponsor this project",
       ],
     ],
-    repo: 'v-poc/rui-playground',
+    repo: "v-poc/rui-playground",
     editLink: true,
     // algolia: {
-    //   apiKey: 'x',
-    //   appId: 'x',
-    //   indexName: 'x',
+    //   apiKey: "x",
+    //   appId: "x",
+    //   indexName: "x",
     // },
     themeConfigByPaths: {
-      '/en': {
-        locale: 'en-US',
-        localeText: 'English',
+      "/en": {
+        locale: "en-US",
+        localeText: "English",
         nav: [
           {
-            text: 'Documentation',
+            text: "Documentation",
             // activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
             items: [
-              { text: 'Organization', link: 'https://github.com/v-poc' },
-              { text: 'Tutorial', link: 'https://nikoni.top/rui-next/docs/' },
-              { text: 'Examples', link: 'https://nikoni.top/rui-next/docs/' },
+              { text: "Organization", link: "https://github.com/v-poc" },
+              { text: "Tutorial", link: "https://nikoni.top/rui-next/docs/" },
+              { text: "Examples", link: "https://nikoni.top/rui-next/docs/" },
             ],
           },
           {
-            text: 'API',
-            icon: 'tabler:cloud-storm',
+            text: "API",
+            icon: "tabler:cloud-storm",
             // activeMatch: `^/api/`,
-            link: 'https://nikoni.top/rui-next/docs/',
+            link: "https://nikoni.top/rui-next/docs/",
           },
           {
-            text: 'Playground',
-            link: 'https://nikoni.top/rui-next/docs/playground.html',
+            text: "Playground",
+            link: "https://nikoni.top/rui-next/docs/playground.html",
           },
           {
-            text: 'Links',
-            icon: 'tabler:mood-smile',
+            text: "Links",
+            icon: "tabler:mood-smile",
             // activeMatch: `^/about/`,
             items: [
               {
-                text: 'Changelog',
-                icon: 'tabler:ghost',
-                link: 'https://github.com/v-poc/rui-next/blob/main/CHANGELOG.md',
+                text: "Changelog",
+                icon: "tabler:ghost",
+                link: "https://github.com/v-poc/rui-next/blob/main/CHANGELOG.md",
               },
               {
-                text: 'Team',
-                icon: 'tabler:users',
-                link: 'https://github.com/orgs/v-poc/people',
+                text: "Team",
+                icon: "tabler:users",
+                link: "https://github.com/orgs/v-poc/people",
               },
               {
-                text: 'Vite',
-                icon: 'tabler:battery-automotive',
-                link: 'https://vitejs.dev/',
+                text: "Vite",
+                icon: "tabler:battery-automotive",
+                link: "https://vitejs.dev/",
               },
+              // {
+              //   text: "Vitepress",
+              //   icon: "tabler:adjustments-horizontal",
+              //   link: "https://vitepress.vuejs.org/",
+              // },
               {
-                text: 'Vitepress',
-                icon: 'tabler:adjustments-horizontal',
-                link: 'https://vitepress.vuejs.org/',
-              },
-              {
-                text: 'RUI(POC)',
-                icon: 'tabler:ghost',
-                link: 'https://github.com/v-poc/rui-poc',
+                text: "RUI(POC)",
+                icon: "tabler:ghost",
+                link: "https://github.com/v-poc/rui-poc",
               },
             ],
           },
           {
-            icon: 'uiw/github',
-            link: 'https://github.com/v-poc/rui-next',
+            icon: "uiw/github",
+            link: "https://github.com/v-poc/rui-next",
           },
         ],
         sidebar: [
           {
-            text: 'RUI',
-            icon: 'openmoji:rocket',
-            link: '/en/getting-started',
+            text: "RUI",
+            icon: "openmoji:rocket",
+            link: "/en/getting-started",
           },
           {
-            text: 'Quick start',
-            link: '/en/getting-started',
+            text: "Quick start",
+            link: "/en/getting-started",
           },
           {
-            text: 'Navigation-Layout',
-            icon: 'lucide:layout',
+            text: "Navigation-Layout",
+            icon: "lucide:layout",
             items: [
               {
-                text: 'Divider',
-                link: '/en/divider/doc',
+                text: "Divider",
+                link: "/en/divider/doc",
               },
               {
-                text: 'AutoJustifyContent',
-                link: '/en/auto-justify-content/doc',
+                text: "AutoJustifyContent",
+                link: "/en/auto-justify-content/doc",
               },
             ],
           },
           {
-            text: 'Experimental',
-            icon: 'lucide:slack',
+            text: "Experimental",
+            icon: "lucide:slack",
             items: [
               {
-                text: 'One Piece',
-                link: '/en/one-piece/doc',
+                text: "One Piece",
+                link: "/en/one-piece/doc",
               },
             ],
           },
