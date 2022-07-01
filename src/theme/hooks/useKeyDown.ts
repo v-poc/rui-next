8042541ad60c0582364614d81c85ba8a0968e350
NikoNi <bluepspower@163.com>
2022-07-02 01:52:10 +0800

    chore: sync changes from pressify (add ci workflow)

--- src/theme/hooks/useKeyDown.ts
+++ src/theme/hooks/useKeyDown.ts
@@ -1,14 +1,15 @@
-import { useEffect, DependencyList } from 'react';
+import { useEffect, DependencyList } from "react";
 
 export function useKeyDown(
   callback: (ev: KeyboardEvent) => void,
   deps: DependencyList = []
 ) {
   useEffect(() => {
-    window.addEventListener('keydown', callback);
+    window.addEventListener("keydown", callback);
 
     return () => {
-      window.removeEventListener('keydown', callback);
+      window.removeEventListener("keydown", callback);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 }
