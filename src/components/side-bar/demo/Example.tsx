edede7637d753ced0a1d1795a3fac8e289ef75e6
NikoNi <bluepspower@163.com>
2023-02-21 01:51:05 +0800

    docs: update SideBar demo

--- src/components/side-bar/demo/Example.tsx
+++ src/components/side-bar/demo/Example.tsx
@@ -1,43 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
 import { SideBar, Divider } from "rui-next";
 import "./index.less";
 
 const arrItems = [
   {
-    key: "key1",
-    title: "item one",
+    key: "wechat",
+    title: "Wechat",
   },
   {
-    key: "key2",
-    title: "item two",
-    badge: "6",
+    key: "alipay",
+    title: "Alipay",
   },
   {
     key: "key3",
-    title: "item three",
+    title: "item3",
     disabled: true,
+    badge: "666",
   },
 ];
 
 // Example FC
-const Example = () => (
-  <>
-    <Divider contentAlign="left">SideBar (badge and disabled state)</Divider>
-    <div className="side-bar-example-wrapper">
-      <div className="side-bar-example-inner">
-        <SideBar>
-          {arrItems.map((item: any, i: number) => (
-            <SideBar.Item
-              key={item.key}
-              title={item.title}
-              disabled={item.disabled}
-              badge={item.badge}
-            />
-          ))}
-        </SideBar>
+const Example = () => {
+  const [activeKey, setActiveKey] = useState("wechat");
+
+  return (
+    <>
+      <Divider contentAlign="left">SideBar (badge and disabled state)</Divider>
+      <div className="side-bar-example-wrapper">
+        <div className="side-bar-example-inner">
+          <div className="side-bar-example-side">
+            <SideBar activeKey={activeKey} onChange={setActiveKey}>
+              {arrItems.map((item: any, i: number) => (
+                <SideBar.Item
+                  key={item.key}
+                  title={item.title}
+                  disabled={item.disabled}
+                  badge={item.badge}
+                />
+              ))}
+            </SideBar>
+          </div>
+          <div className="side-bar-example-main">
+            <div
+              className={classnames("side-bar-example-content", {
+                "side-bar-example-active": activeKey === "alipay",
+              })}
+            >
+              <img src="https://nikoni.top/images/alipay-qrcode.png" />
+            </div>
+            <div
+              className={classnames("side-bar-example-content", {
+                "side-bar-example-active": activeKey === "wechat",
+              })}
+            >
+              <img src="https://nikoni.top/images/niko-reward-qrcode.png" />
+            </div>
+            <div className="side-bar-example-content">Three content</div>
+          </div>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Example;
